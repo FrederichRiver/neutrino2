5bfaaf6e5e55fea4da39e15709bd3f7f7fd6271c
FrederichRiver <hezhiyuan_tju@sina.com>
2020-07-02 00:00:45 +0800

    lib dev_global update

---
+++ dev_global/dev_global/env.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+# from polaris.mysql8 import mysqlHeader
+
+"""
+global environment varibles
+
+"""
+
+PYTHON_VERSION = 3.6
+LOCAL_TIME_ZONE = 'Beijing'
+TIME_FMT = '%Y-%m-%d'
+
+
+if os.getenv('SERVER') == 'MARS':
+    ROOT_PATH = '/root/'
+    SOFT_PATH = '/opt/neutrino/'
+else:
+    ROOT_PATH = '~/Dev/neutrino2/'
+    SOFT_PATH = '~/Dev/neutrino2/'
+
+
+PROJ_PATH = '/home/friederich/Dev/neutrino2'
+
+encode = 'wAKO0tFJ8ZH38RW4WseZnQ=='
+
+LOG_FILE = SOFT_PATH + 'neutrino.log'
+PID_FILE = '/tmp/neutrino.pid'
+TASK_FILE = SOFT_PATH + 'config/task.json'
+CONF_FILE = SOFT_PATH + 'config/conf.json'
+HEAD_FILE = SOFT_PATH + 'config/header.json'
+COOKIE_FILE = SOFT_PATH + 'config/cookie.json'
+SQL_FILE = SOFT_PATH + 'config/sql.json'
+MANUAL = SOFT_PATH + 'config/Neutrino'
+
+
+# GLOBAL_HEADER = mysqlHeader('stock', 'stock2020', 'stock')
+# VIEWER_HEADER = mysqlHeader('view', 'view2020', 'stock')
